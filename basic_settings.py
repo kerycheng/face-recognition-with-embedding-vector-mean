2b98bbace1806db72c3c64d5fee4007d2af743ca
ChengYanKai <chengyankai1216@gmail.com>
2023-01-03 15:49:45 +0800

    Add files via upload

--- basic_settings.py
+++ basic_settings.py
@@ -99,21 +99,105 @@ class calculation(setup_settings):
         self.embs_list = []
         self.images_name_list = []
 
-        for i in range(1):
+        for i in range(len(self.dataset_dir)):
             dataset_images_name = os.path.join(self.dataset_path, str(self.dataset_dir[i]))  # 讀目錄底下圖片名稱
             dataset_images_path = sorted(
                 [os.path.join(dataset_images_name, f) for f in os.listdir(dataset_images_name)])  # 目錄路徑 + 圖片名稱之後做排序
             self.images_name_list.append(sorted([f for f in os.listdir(dataset_images_name)])) # 將每張圖片名稱放入images_name_list
             self.embs_list.append([self.get_embs(img) for img in dataset_images_path])  # 計算embs放入embs_list
 
-    # 自身emb2emb表
+    # 取得每個人的代表向量
+    def get_vector_mean(self):
+        self.vector_mean_list = [self.l2_normalize(np.divide(sum(np.array(self.embs_list[i])), len(self.embs_list[i]))) for i in range(len(self.embs_list))]
+
+    # 自身特徵向量與自身特徵向量比對
     def self_emb2emb_list(self):
-        self.self_emb2emb = []
+        self.eu_self_emb2emb = []
+        self.ip_self_emb2emb = []
 
         for embs in self.embs_list:
-            ddl = [self.euclidean_distance(embs[i], embs[j]) for (i, j) in itertools.combinations(range(len(embs)), 2)]
-            self.self_emb2emb.append((min(ddl), max(ddl), np.std(ddl, ddof=1), np.mean(ddl)))
+            eul = [self.euclidean_distance(embs[i], embs[j]) for (i, j) in itertools.combinations(range(len(embs)), 2)]
+            ipl = [self.innerproduct(embs[i], embs[j]) for (i, j) in itertools.combinations(range(len(embs)), 2)]
+            self.eu_self_emb2emb.append((min(eul), max(eul), np.std(eul, ddof=1), np.mean(eul)))
+            self.ip_self_emb2emb.append((min(ipl), max(ipl), np.std(ipl, ddof=1), np.mean(ipl)))
             # 最短距離、最遠距離、標準差距離、平均距離
 
-    def other_calculation_list(self):
-        return 0
+    # 自身特徵向量與他人特徵向量比對
+    def other_emb2emb_list(self):
+        self.eu_other_emb2emb = []
+        self.ip_other_emb2emb = []
+        eu_self_emb2emb_min = [self.eu_self_emb2emb[i][0] for i in range(len(self.eu_self_emb2emb))]
+        ip_self_emb2emb_min = [self.ip_self_emb2emb[i][0] for i in range(len(self.ip_self_emb2emb))]
+
+        for i in range(len(self.embs_list)):
+            # eoed = eu_other_emb2emb_distance
+            # ioed = ip_other_emb2emb_distance
+            eoed = []
+            ioed = []
+
+            for j in range(len(self.embs_list[i])):
+                eul = min([self.euclidean_distance(self.embs_list[i][n], self.embs_list[j][m])
+                           for (n, m) in itertools.combinations(range(len(self.embs_list[i])), 2)])
+                ipl = min([self.innerproduct(self.embs_list[i][n], self.embs_list[j][m])
+                           for (n, m) in itertools.combinations(range(len(self.embs_list[i])), 2)])
+                # 把min當作該人的代表向量
+                if eul == eu_self_emb2emb_min[i]:  # 如果 ddl == 自己的 min_ed 則 pass
+                    pass
+                else:
+                    eoed.append(eul)
+
+                if ipl == ip_self_emb2emb_min[i]:  # 如果 ddl == 自己的 min_ed 則 pass
+                    pass
+                else:
+                    ioed.append(ipl)
+
+            self.eu_other_emb2emb.append((min(eoed), max(eoed), np.std(eoed, ddof=1), np.mean(eoed)))
+            self.ip_other_emb2emb.append((min(ioed), max(ioed), np.std(ioed, ddof=1), np.mean(ioed)))
+
+    # 自身代表向量(vector mean)與自身特徵向量比對
+    def self_vm2emb_list(self):
+        self.eu_self_vm2emb = []
+        self.ip_self_vm2emb = []
+
+        for i in range(len(self.vector_mean_list)):
+            # eu_self_vm2emb_distance
+            # ip_self_vm2emb_distance
+            esvd = []
+            isvd = []
+
+            for j in range(len(self.embs_list[i])):
+                eul = [self.euclidean_distance(self.vector_mean_list[i], self.embs_list[i][j])]
+                ipl = [self.innerproduct(self.vector_mean_list[i], self.embs_list[i][j])]
+                esvd.append(eul)
+                isvd.append(ipl)
+            self.eu_self_vm2emb.append(((min(esvd)[0]), (max(esvd)[0]), np.std(esvd, ddof=1), np.mean(esvd)))
+            self.ip_self_vm2emb.append(((min(isvd)[0]), (max(isvd)[0]), np.std(isvd, ddof=1), np.mean(isvd)))
+
+    # 自身代表向量禹他人特徵向量比對
+    def other_vm2emb_list(self):
+        self.eu_other_vm2emb = []
+        self.ip_other_vm2emb = []
+        eu_self_vector_mean_min = [(self.eu_self_vm2emb[i][0]) for i in range(len(self.eu_self_vm2emb))]
+        ip_self_vector_mean_min = [(self.ip_self_vm2emb[i][0]) for i in range(len(self.ip_self_vm2emb))]
+
+        for i in range(len(self.vector_mean_list)):
+            # eu_other_vm2emb_distance
+            # ip_other_vm2emb_distance
+            eovd = []
+            iovd = []
+            for j in range(len(self.embs_list)):
+                eul = min([self.euclidean_distance(self.vector_mean_list[i], self.embs_list[j][m]) for m in range(len(self.embs_list[i]))])
+                ipl = min([self.innerproduct(self.vector_mean_list[i], self.embs_list[j][m]) for m in range(len(self.embs_list[i]))])
+
+                if eul == eu_self_vector_mean_min[i]:
+                    pass
+                else:
+                    eovd.append(eul)
+
+                if ipl == ip_self_vector_mean_min[i]:
+                    pass
+                else:
+                    iovd.append(ipl)
+
+            self.eu_other_vm2emb.append((min(eovd), max(eovd), np.mean(eovd), np.std(eovd, ddof=1)))
+            self.ip_other_vm2emb.append((min(iovd), max(iovd), np.mean(iovd), np.std(iovd, ddof=1)))
--- create_dataframe.py
+++ create_dataframe.py
@@ -1,31 +1,75 @@
 import warnings
+
+import pandas as pd
+
 warnings.filterwarnings('ignore')
 
-import os
-import tensorflow as tf
 import numpy as np
 np.set_printoptions(suppress=True)
 
-import pandas as pd
-import scipy.misc
-import itertools
-import random
-import cv2
-import facenet
-import detect_face
-
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-import visualization_utils as vis_utils
-from sklearn.cluster import MeanShift, estimate_bandwidth
-
-from basic_settings import setup_settings
 from basic_settings import calculation
 
-class create_dataframe(setup_settings, calculation):
+class create_dataframe(calculation):
     def __init__(self):
         super().__init__()
 
+    def run(self):
+        ca = calculation()
+        self.data = ca.data_path
 
+        ca.get_dir_all_embs()
+        self.name = ca.dataset_dir
+        self.embeddings = ca.embs_list
+
+        ca.get_vector_mean()
+        self.vector_mean = ca.vector_mean_list
+
+        ca.self_emb2emb_list()
+        self.ese2e = ca.eu_self_emb2emb
+        self.ise2e = ca.ip_self_emb2emb
+
+        ca.other_emb2emb_list()
+        self.eoe2e = ca.eu_other_emb2emb
+        self.ioe2e = ca.ip_other_emb2emb
+
+        ca.self_vm2emb_list()
+        self.esv2e = ca.eu_self_vm2emb
+        self.isv2e = ca.ip_self_vm2emb
+
+        ca.other_vm2emb_list()
+        self.eov2e = ca.eu_other_vm2emb
+        self.iov2e = ca.ip_other_vm2emb
+
+        self.create_dataframe()
+        self.read_dataframe()
+
+    def create_dataframe(self):
+        dict = {
+            'People Name': self.name,
+            'Embeddings': self.embeddings,
+            'Vector Mean': self.vector_mean,
+            'EU Self E2E': self.ese2e,
+            'IP Self E2E': self.ise2e,
+            'EU Other E2E': self.eoe2e,
+            'IP Other E2E': self.ioe2e,
+            'EU Self V2E': self.esv2e,
+            'IP Self V2E': self.isv2e,
+            'EU Other V2E': self.eov2e,
+            'IP Other V2E': self.iov2e
+        }
+        df = pd.DataFrame(dict)
+        df.to_json(f'{self.data}/dataset.json')
+
+    def read_dataframe(self):
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', None)
+        pd.set_option('display.max_colwidth', -1)
+
+        df = pd.read_json(f'{self.data}/dataset.json')
+        print(df)
+
+if __name__ == '__main__':
+    cd = create_dataframe()
+    cd.run()
 
